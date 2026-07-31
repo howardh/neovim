8642e45b98d2548864dbee450cd82f883173951f
Howard <174055+howardh@users.noreply.github.com>
2026-07-31 11:49:43 -0400

    Config styling and nvim version check

--- init.lua
+++ init.lua
@@ -1,42 +1,42 @@
 local vim = vim
 local Plug = vim.fn['plug#'] -- TODO: Switch to lazy.vim. Documentation for VimPlug are becoming increasingly sparse.
 
+local hostname = vim.fn.hostname()
 local MINUET_ENDPOINT = os.getenv('MINUET_ENDPOINT');
 
+-- Plugins
 vim.call('plug#begin')
-
--- Pastel Colour Schemes
-Plug('catppuccin/nvim', {['as'] = 'catppuccin'})
-
--- Folding
--- You can check the file's current filetype with `:set filetype?`
-Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
-Plug('tmhedberg/SimpylFold', { ['for'] = { 'python' } }) -- Python folding
-Plug('pangloss/vim-javascript', { ['for'] = { 'javascript', 'javascriptreact' } })
-Plug('maxmellon/vim-jsx-pretty', { ['for'] = { 'javascript', 'javascriptreact' } })
-
--- Misc
-Plug('neoclide/coc.nvim', {['branch'] = 'release'}) -- Autocomplete
-Plug('skwp/greplace.vim') -- Search/Replace
---Plug('vim-ctrlspace/vim-ctrlspace')
-Plug('vim-airline/vim-airline') -- Tab line
-Plug('preservim/nerdtree')
---Plug('github/copilot.vim', { ['on'] = 'Copilot' })
-
-Plug(
-  'milanglacier/minuet-ai.nvim',
-   MINUET_ENDPOINT and {} or { ['on'] = {} }
-)
---Plug('hrsh7th/nvim-cmp')       -- optional
---Plug('Saghen/blink.cmp')       -- optional
-
-Plug('nvim-lua/plenary.nvim') -- Telescope dependency
-Plug('nvim-telescope/telescope.nvim', { ['branch'] = 'master' }) -- Fuzzy finder
--- Also might want to install https://github.com/BurntSushi/ripgrep. I don't
--- know what it does, but a side-effect is that .gitignore files are respected,
--- which is all I care about. This needs to be installed via the system's
--- package manager, e.g. `sudo pacman -S ripgrep`.
-
+  -- Pastel Colour Schemes
+  Plug('catppuccin/nvim', {['as'] = 'catppuccin'})
+  
+  -- Folding
+  -- You can check the file's current filetype with `:set filetype?`
+  Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
+  Plug('tmhedberg/SimpylFold', { ['for'] = { 'python' } }) -- Python folding
+  Plug('pangloss/vim-javascript', { ['for'] = { 'javascript', 'javascriptreact' } })
+  Plug('maxmellon/vim-jsx-pretty', { ['for'] = { 'javascript', 'javascriptreact' } })
+  
+  -- Misc
+  Plug('neoclide/coc.nvim', {['branch'] = 'release'}) -- Autocomplete (Requires neovim >= 0.8)
+  Plug('skwp/greplace.vim') -- Search/Replace
+  --Plug('vim-ctrlspace/vim-ctrlspace')
+  Plug('vim-airline/vim-airline') -- Tab line
+  Plug('preservim/nerdtree')
+  --Plug('github/copilot.vim', { ['on'] = 'Copilot' })
+  
+  Plug(
+    'milanglacier/minuet-ai.nvim',
+     MINUET_ENDPOINT and {} or { ['on'] = {} }
+  )
+  --Plug('hrsh7th/nvim-cmp')       -- optional
+  --Plug('Saghen/blink.cmp')       -- optional
+  
+  Plug('nvim-lua/plenary.nvim') -- Telescope dependency
+  Plug('nvim-telescope/telescope.nvim', { ['branch'] = 'master' }) -- Fuzzy finder
+  -- Also might want to install https://github.com/BurntSushi/ripgrep. I don't
+  -- know what it does, but a side-effect is that .gitignore files are respected,
+  -- which is all I care about. This needs to be installed via the system's
+  -- package manager, e.g. `sudo pacman -S ripgrep`.
 vim.call('plug#end')
 
 
@@ -54,6 +54,15 @@ vim.opt.splitright = true     -- Open new vertical splits to the right of curren
 
 vim.opt.breakindent = true
 vim.opt.showbreak = ">> "
+
+
+-- Validation
+-- This happens after the general settings
+if vim.keymap == nil then
+  local vim_version_str = vim.version().major .. "." .. vim.version().minor
+  vim.notify("`vim.keymap` is nil. Requires Neovim>0.7. Currently on version " .. vim_version_str .. ".", vim.log.levels.ERROR)
+  return
+end
 
 
 -- https://neovim.io/doc/user/lua-guide.html#lua-guide-mappings-set
