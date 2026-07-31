071053e788e901206a86169740279c1144a54204
Howard <174055+howardh@users.noreply.github.com>
2026-07-31 11:12:41 -0400

    Colour scheme, file type, and Minuet AI

--- init.lua
+++ init.lua
@@ -1,13 +1,19 @@
 local vim = vim
-local Plug = vim.fn['plug#']
+local Plug = vim.fn['plug#'] -- TODO: Switch to lazy.vim. Documentation for VimPlug are becoming increasingly sparse.
+
+local MINUET_ENDPOINT = os.getenv('MINUET_ENDPOINT');
 
 vim.call('plug#begin')
 
+-- Pastel Colour Schemes
+Plug('catppuccin/nvim', {['as'] = 'catppuccin'})
+
 -- Folding
+-- You can check the file's current filetype with `:set filetype?`
 Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
-Plug('tmhedberg/SimpylFold') -- Python folding
-Plug('pangloss/vim-javascript')
-Plug('maxmellon/vim-jsx-pretty')
+Plug('tmhedberg/SimpylFold', { ['for'] = { 'python' } }) -- Python folding
+Plug('pangloss/vim-javascript', { ['for'] = { 'javascript', 'javascriptreact' } })
+Plug('maxmellon/vim-jsx-pretty', { ['for'] = { 'javascript', 'javascriptreact' } })
 
 -- Misc
 Plug('neoclide/coc.nvim', {['branch'] = 'release'}) -- Autocomplete
@@ -15,10 +21,17 @@ Plug('skwp/greplace.vim') -- Search/Replace
 --Plug('vim-ctrlspace/vim-ctrlspace')
 Plug('vim-airline/vim-airline') -- Tab line
 Plug('preservim/nerdtree')
-Plug('github/copilot.vim', { ['on'] = 'Copilot' })
+--Plug('github/copilot.vim', { ['on'] = 'Copilot' })
+
+Plug(
+  'milanglacier/minuet-ai.nvim',
+   MINUET_ENDPOINT and {} or { ['on'] = {} }
+)
+--Plug('hrsh7th/nvim-cmp')       -- optional
+--Plug('Saghen/blink.cmp')       -- optional
 
 Plug('nvim-lua/plenary.nvim') -- Telescope dependency
-Plug('nvim-telescope/telescope.nvim', { ['branch'] = '0.1.x' }) -- Fuzzy finder
+Plug('nvim-telescope/telescope.nvim', { ['branch'] = 'master' }) -- Fuzzy finder
 -- Also might want to install https://github.com/BurntSushi/ripgrep. I don't
 -- know what it does, but a side-effect is that .gitignore files are respected,
 -- which is all I care about. This needs to be installed via the system's
@@ -44,6 +57,21 @@ vim.opt.showbreak = ">> "
 
 
 -- https://neovim.io/doc/user/lua-guide.html#lua-guide-mappings-set
+
+
+-- Colour scheme
+require("catppuccin").setup {
+    custom_highlights = function(colors)
+        return {
+            -- See available colours here: https://github.com/catppuccin/catppuccin/blob/main/docs/style-guide.md
+            WinSeparator = { fg = colors.overlay0 }, -- Make the vertical pane serparators visible
+        }
+    end,
+    integrations = {
+      coc_nvim = true,
+    }
+}
+vim.cmd.colorscheme "catppuccin-mocha"
 
 
 -- CoC Settings
@@ -83,3 +111,64 @@ vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { noremap = tr
 -- Copilot settings
 -- Disable by default. It can be re-enabled as needed with `:Copilot enable`.
 vim.g.copilot_enabled = false
+
+-- Minuet-ai settings
+if MINUET_ENDPOINT then
+  require('minuet').setup {
+      provider = 'openai_fim_compatible',
+      n_completions = 1, -- recommend for local model for resource saving
+      -- I recommend beginning with a small context window size and incrementally
+      -- expanding it, depending on your local computing power. A context window
+      -- of 512, serves as an good starting point to estimate your computing
+      -- power. Once you have a reliable estimate of your local computing power,
+      -- you should adjust the context window to a larger value.
+      context_window = 512,
+      provider_options = {
+          openai_fim_compatible = {
+              -- For Windows users, TERM may not be present in environment variables.
+              -- Consider using APPDATA instead.
+              api_key = 'TERM',
+              name = 'Llama.cpp',
+              --end_point = 'http://192.168.0.33:8012/v1/completions',
+              end_point = MINUET_ENDPOINT,
+              -- The model is set by the llama-cpp server and cannot be altered
+              -- post-launch.
+              model = 'PLACEHOLDER',
+              optional = {
+                  max_tokens = 56,
+                  top_p = 0.9,
+              },
+              -- Llama.cpp does not support the `suffix` option in FIM completion.
+              -- Therefore, we must disable it and manually populate the special
+              -- tokens required for FIM completion.
+              template = {
+                  prompt = function(context_before_cursor, context_after_cursor, _)
+                      return '<|fim_prefix|>'
+                          .. context_before_cursor
+                          .. '<|fim_suffix|>'
+                          .. context_after_cursor
+                          .. '<|fim_middle|>'
+                  end,
+                  suffix = false,
+              },
+          },
+      },
+      virtualtext = {
+          auto_trigger_ft = {'python'},
+          keymap = {
+              -- accept whole completion
+              accept = '<A-A>',
+              -- accept one line
+              accept_line = '<A-a>',
+              -- accept n lines (prompts for number)
+              -- e.g. "A-z 2 CR" will accept 2 lines
+              accept_n_lines = '<A-z>',
+              -- Cycle to prev completion item, or manually invoke completion
+              prev = '<A-[>',
+              -- Cycle to next completion item, or manually invoke completion
+              next = '<A-]>',
+              dismiss = '<A-e>',
+          },
+      },
+  }
+end
